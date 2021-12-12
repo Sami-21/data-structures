45202ba94062b424d4ba5a27686a522cff19852d
Sami Maachi <samovitch360@gmail.comgit config --global user.name Sami>
2021-12-12 19:18:50 +0100

    testing my contribution history

--- Hash Tables/HashTables.js
+++ Hash Tables/HashTables.js
@@ -1,21 +1,56 @@
 //The Hash Tables are represented as Objects in javascript.
 //Small example on the Objects in js
 
-let Person = {
-    FullName: "Random Name",
-    height: 1.86,
-    weight: 82,
-    Age: 35,
-    nationality: "algerian",
-    alive: true,
-    occupation: "Web Developer",
-    FavoriteLine: () => {
-        console.log("I am learning Data Structures :^)")
+// let Person = {
+//     FullName: "Random Name",
+//     height: 1.86,
+//     weight: 82,
+//     Age: 35,
+//     nationality: "algerian",
+//     alive: true,
+//     occupation: "Web Developer",
+//     FavoriteLine: () => {
+//         console.log("I am learning Data Structures :^)")
+//     }
+// };
+
+// console.log(Person.weight); // O(1)
+
+// Person.address = "103.163.101.0"; // O(1)
+
+// Person.FavoriteLine(); // O(1)
+
+////////////////////////////////////////////////////////////////////////////////////
+
+// Let's manually create HashTable
+
+class HashTable {
+    constructor(size){
+        this.data = new Array(size);
     }
-};
 
-console.log(Person.weight); // O(1)
+    _hash(key){
+        let hash = 0;
+        for (let i = 0;i<key.length;i++){
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length ;
+        }
+        return hash;
+    }
+    set(key,value){
+        if(this.data[this.data.length-1]!==undefined) return console.log("the map is full")
+        for(let i=0;i<this.data.length;i++){
+            if(this.data[i]===undefined){
+                this.data[i] = [key,value];
+                break;
+            }
+        }
+    }
+    get(key){
+        for(let i=0;i<this.data.length;i++){
+            if(this.data[i][0]===key) return console.log(this.data[i][1]);
+            break;
+        }
+        return console.log("element not found");
+    }
+}
 
-Person.address = "103.163.101.0"; // O(1)
-
-Person.FavoriteLine(); // O(1)
